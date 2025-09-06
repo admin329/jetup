1c7ed5e2db10ca33dd03f0b512aeb956108fe486
JETUP <admin@jetup.aero>
2025-09-07 00:58:07 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,18 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
+    outDir: 'dist',
+    sourcemap: false,
+    minify: 'esbuild',
     rollupOptions: {
       input: {
         main: './index.html'
+      },
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          router: ['react-router-dom']
+        }
       }
     }
   },
