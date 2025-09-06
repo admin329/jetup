b10b65d94b64888c73c1d7e641a2f6ff2fc0a9dc
JETUP <admin@jetup.aero>
2025-09-06 23:56:47 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,22 +3,9 @@ import { createRoot } from 'react-dom/client';
 import { HelmetProvider } from 'react-helmet-async';
 import App from './App.tsx';
 import './index.css';
-import { initializeEnvironment } from './config/environment';
-    <HelmetProvider>
-      <App />
-    </HelmetProvider>
-import { setupGlobalErrorHandler } from './utils/errorHandler';
-import { initializeSecurity } from './utils/security';
-import { initializeTwoFactorService } from './services/twoFactorService';
 import { AuthProvider } from './contexts/AuthContext';
 import { InvoiceProvider } from './contexts/InvoiceContext';
 import { SupabaseAuthProvider } from './contexts/SupabaseAuthContext';
-
-// Initialize production environment
-initializeEnvironment();
-setupGlobalErrorHandler();
-initializeSecurity();
-initializeTwoFactorService();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
