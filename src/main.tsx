85ed7f87f182597d20a0d8c59d6014491510c458
JETUP <admin@jetup.aero>
2025-09-06 21:00:41 +0300

    Create main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,35 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { HelmetProvider } from 'react-helmet-async';
+import App from './App.tsx';
+import './index.css';
+import './test/paymentTest';
+import './test/emailTest';
+import './test/stripeTest';
+import { initializeEnvironment } from './config/environment';
+import { setupGlobalErrorHandler } from './utils/errorHandler';
+import { initializeSecurity } from './utils/security';
+import { initializeTwoFactorService } from './services/twoFactorService';
+import { AuthProvider } from './contexts/AuthContext';
+import { InvoiceProvider } from './contexts/InvoiceContext';
+import { SupabaseAuthProvider } from './contexts/SupabaseAuthContext';
+
+// Initialize production environment
+initializeEnvironment();
+setupGlobalErrorHandler();
+initializeSecurity();
+initializeTwoFactorService();
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <HelmetProvider>
+      <SupabaseAuthProvider>
+        <AuthProvider>
+          <InvoiceProvider>
+            <App />
+          </InvoiceProvider>
+        </AuthProvider>
+      </SupabaseAuthProvider>
+    </HelmetProvider>
+  </StrictMode>
+);
